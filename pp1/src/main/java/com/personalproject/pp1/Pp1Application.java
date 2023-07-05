33cc2866962c3f5c02679b6cb4f74eab3ccbe384
jayanth <jayanth@localhost>
2023-07-04 23:41:15 -0400

    initial commit

---
+++ pp1/src/main/java/com/personalproject/pp1/Pp1Application.java
@@ -0,0 +1,13 @@
+package com.personalproject.pp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp1Application.class, args);
+	}
+
+}
